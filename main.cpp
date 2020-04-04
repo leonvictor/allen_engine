0d536a1b064ef7cbb2f25720ce922c806abaff73
Léon <levi486@gmail.com>
2020-04-04 20:31:10 +0200

    Tutorial: Framebuffers

--- main.cpp
+++ main.cpp
@@ -93,6 +93,7 @@ private:
     VkPipelineLayout pipelineLayout;
     VkPipeline graphicsPipeline;
 
+    std::vector<VkFramebuffer> swapchainFramebuffers;
 
     bool checkValidationLayersSupport() {
         uint32_t layerCount;
@@ -259,6 +260,31 @@ private:
         createImageViews();
         createRenderPass();
         createGraphicsPipeline();
+        createFramebuffers();
+    }
+
+    void createFramebuffers() {
+        swapchainFramebuffers.resize(swapchainImageViews.size());
+
+        for (size_t i = 0; i < swapchainImages.size(); i++) {
+            VkImageView attachments[] = {
+                swapchainImageViews[i]
+            };
+
+            VkFramebufferCreateInfo framebufferInfo = {};
+            framebufferInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+            framebufferInfo.renderPass = renderPass;
+            framebufferInfo.attachmentCount = 1; // For now
+            framebufferInfo.pAttachments = attachments;
+            framebufferInfo.width = swapchainExtent.width;
+            framebufferInfo.height = swapchainExtent.height;
+            framebufferInfo.layers = 1; // Nb of layers in image array.
+
+            if (vkCreateFramebuffer(device, &framebufferInfo, nullptr, &swapchainFramebuffers[i]) != VK_SUCCESS) {
+                throw std::runtime_error("Failed to create Framebuffer.");
+            }
+        }
+
     }
 
     void createRenderPass() {
@@ -733,6 +759,10 @@ private:
     }
 
     void cleanup() {
+        for (auto framebuffer : swapchainFramebuffers) {
+            vkDestroyFramebuffer(device, framebuffer, nullptr);
+        }
+
         vkDestroyPipeline(device, graphicsPipeline, nullptr);
         vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
         vkDestroyRenderPass(device, renderPass, nullptr);
