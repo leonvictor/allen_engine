6b4f77f43baf61b371020ca62640a62d53557476
Léon <levi486@gmail.com>
2020-07-09 11:01:58 +0200

    Take objects rotation into account

--- src/scene_object.cpp
+++ src/scene_object.cpp
@@ -49,8 +49,13 @@ class SceneObject
 
     glm::mat4 getModelMatrix()
     {
-        // TODO: Add rotation
-        return glm::translate(glm::scale(modelMatrix, transform.scale), transform.position);
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, transform.position);
+        model = glm::rotate(model, glm::radians(transform.rotation.x), glm::vec3(1, 0, 0));
+        model = glm::rotate(model, glm::radians(transform.rotation.y), glm::vec3(0, 1, 0));
+        model = glm::rotate(model, glm::radians(transform.rotation.z), glm::vec3(0, 0, 1));
+        model = glm::scale(model, transform.scale);
+        return model;
     }
 
     void addMaterial(MaterialBufferObject newMaterial)
@@ -113,7 +118,4 @@ class SceneObject
 
         device->logical.get().updateDescriptorSets(static_cast<uint32_t>(writeDescriptors.size()), writeDescriptors.data(), 0, nullptr);
     }
-
-  private:
-    glm::mat4 modelMatrix = glm::mat4(1.0f); // TODO
 };
