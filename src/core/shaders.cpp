9ff7e9a3cf1b5ad4c2e735d4ad22616536fac40b
Léon <levi486@gmail.com>
2020-12-27 03:19:29 +0100

    Add support for runtime shader compilation

--- src/core/shaders.cpp
+++ src/core/shaders.cpp
@@ -1,14 +1,30 @@
 #include "shaders.hpp"
 #include "../utils/files.cpp"
 #include "device.hpp"
+#include <iostream>
 #include <vulkan/vulkan.hpp>
 
 namespace core::shaders
 {
+
 vk::ShaderModule createShaderModule(std::shared_ptr<core::Device> device, const std::string& filename)
 {
-    auto code = utils::readFile(filename);
-    return device->logical.get().createShaderModule({{}, code.size(), reinterpret_cast<const uint32_t*>(code.data())});
+    std::string ext = utils::getFileExtension(filename);
+
+    if (ext == "spv")
+    {
+        std::vector<char> code = utils::readFile(filename);
+        return device->logical.get().createShaderModule({{}, code.size() * sizeof(char), reinterpret_cast<uint32_t*>(code.data())});
+    }
+    else if (ext == "vert" || ext == "frag")
+    {
+        std::vector<uint32_t> code = compileGlslToSpvBinary(filename, shaderc_glsl_infer_from_source, false);
+        return device->logical.get().createShaderModule({{}, code.size() * sizeof(uint32_t), code.data()});
+    }
+    else
+    {
+        throw;
+    }
 }
 
 vk::PipelineShaderStageCreateInfo loadShader(std::shared_ptr<core::Device> device, const std::string& filename, vk::ShaderStageFlagBits stage, const std::string& entryPoint)
@@ -21,5 +37,39 @@ vk::PipelineShaderStageCreateInfo loadShader(std::shared_ptr<core::Device> devic
     // pSpecializationInfo : We can set values for constants in the shader.
     // Then we can use a single shader module and have its behavior configured at pipeline creation (here)
     return createInfo;
+}
+
+/// @brief Compiles a shader to a SPIR-V binary.
+///
+/// @param source_name glsl shader file name
+/// @param kind shaderc kind of shader (vertex, frag, compute)
+/// @param optimize whether to optimize the shader
+///
+/// @return the binary as a vector of 32-bit words.
+std::vector<uint32_t> compileGlslToSpvBinary(const std::string& source_name,
+                                             shaderc_shader_kind kind,
+                                             bool optimize)
+{
+    shaderc::Compiler compiler;
+    shaderc::CompileOptions options;
+
+    options.SetTargetEnvironment(shaderc_target_env_vulkan, shaderc_env_version_vulkan_1_2);
+
+    std::vector<char> source = utils::readFile(source_name);
+
+    if (optimize)
+        options.SetOptimizationLevel(shaderc_optimization_level_size);
+
+    shaderc::SpvCompilationResult module =
+        compiler.CompileGlslToSpv(std::string(source.begin(), source.end()), kind, source_name.c_str(), options);
+
+    if (module.GetCompilationStatus() != shaderc_compilation_status_success)
+    {
+        std::cerr << module.GetErrorMessage();
+        // TODO: Custom exception type (something like ShaderCompilationException)
+        throw;
+    }
+
+    return {module.cbegin(), module.cend()};
 }
 } // namespace core::shaders
--- src/core/shaders.hpp
+++ src/core/shaders.hpp
@@ -2,10 +2,13 @@
 
 #include "../utils/files.cpp"
 #include "device.hpp"
-#include <vulkan/vulkan.hpp>
+#include <shaderc/shaderc.hpp>
 
 namespace core::shaders
 {
 vk::ShaderModule createShaderModule(std::shared_ptr<core::Device> device, const std::string& filename);
 vk::PipelineShaderStageCreateInfo loadShader(std::shared_ptr<core::Device> device, const std::string& filename, vk::ShaderStageFlagBits stage, const std::string& entryPoint);
+std::vector<uint32_t> compileGlslToSpvBinary(const std::string& source_name,
+                                             shaderc_shader_kind kind,
+                                             bool optimize = false);
 } // namespace core::shaders
