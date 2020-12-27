a8d909b7966fab2082b6a00193ef8bcb88731ae4
Léon <levi486@gmail.com>
2020-12-27 23:03:36 +0100

    Make transform rotation a quaternion internally

--- src/transform.hpp
+++ src/transform.hpp
@@ -2,10 +2,12 @@
 
 #include "components.hpp"
 #include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/quaternion.hpp>
 
 struct Transform : public Component
 {
     glm::vec3 position = glm::vec3(0.0f);
-    glm::vec3 rotation = glm::vec3(0.0f);
+    glm::quat rotation = glm::quat();
     glm::vec3 scale = glm::vec3(1.0f);
 };
