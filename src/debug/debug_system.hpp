c8a385434a678744202595f84c3ecd75222b01c6
Léon <levi486@gmail.com>
2021-06-02 13:36:23 +0200

    Create mostly empty debug system

---
+++ src/debug/debug_system.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "../utils/singleton.hpp"
+
+#include <iostream>
+#include <string>
+
+enum Level
+{
+    Info,
+    Warning,
+    Error,
+};
+
+/// @brief Debug utility for the Engine.
+/// @todo For now these utility simply print the logs to the console
+class Debug : private ISingleton<Debug>
+{
+    /// @brief Log a message at the info level.
+    static void Log(std::string text)
+    {
+        std::cout << text << std::endl;
+    }
+
+    /// @brief Log a warning
+    static void LogWarning(std::string text)
+    {
+        std::cout << text << std::endl;
+    }
+
+    /// @brief Log an error
+    static void LogError(std::string text)
+    {
+        std::cout << text << std::endl;
+    }
+};
