e518b7d6069f83a35414d5adf6837b6b79ec5d9e
Léon <levi486@gmail.com>
2021-11-10 09:52:15 +0100

    Improve asset system to use a handle class

--- src/assets/include/assets/asset.hpp
+++ src/assets/include/assets/asset.hpp
@@ -8,11 +8,14 @@ namespace aln
 using AssetGUID = std::string;
 
 /// @brief Abstract base class for all assets (textures, meshes, animation...).
-/// Does not provide much behavior but allows us to restict the user to only using the "right" asset types
-/// @warning Most of the implementation is not done ! Only inherit from this for now.
 class IAsset
 {
+    friend class AssetLoader;
+
   private:
+    AssetGUID m_id;
+
+  protected:
     enum class Status
     {
         Unloaded,   // Object created and attributes set
@@ -20,13 +23,16 @@ class IAsset
         Initialized // Asset loaded and initialization step done
     };
 
-    Status m_status = Status::Unloaded; // TODO: Unused
-    AssetGUID m_id;                     // TODO: Let the asset loader set the ID
+    Status m_status = Status::Unloaded;
+    IAsset(AssetGUID& guid) : m_id(guid) {}
 
   public:
-    const AssetGUID& GetID() { return m_id; }
-    bool IsUnloaded() { return m_status == Status::Unloaded; }
-    bool IsLoaded() { return m_status == Status::Loaded || m_status == Status::Initialized; }
-    bool IsInitialized() { return m_status == Status::Initialized; }
+    const AssetGUID& GetID() const { return m_id; }
+    bool IsUnloaded() const { return m_status == Status::Unloaded; }
+    bool IsLoaded() const { return m_status == Status::Loaded; }
+    bool IsInitialized() const { return m_status == Status::Initialized; }
 };
+
+template <class T>
+concept AssetType = std::is_base_of<IAsset, T>::value;
 } // namespace aln
