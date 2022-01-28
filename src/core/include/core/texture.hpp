80390645d98983f36904e87f5b6c8c9a455bbd01
Léon <levi486@gmail.com>
2022-01-28 18:02:16 +0100

    Change missing mesh component rename

--- src/core/include/core/texture.hpp
+++ src/core/include/core/texture.hpp
@@ -10,7 +10,7 @@ class Texture : public IAsset
 {
     // TODO: friends shouldn't be necessary
     friend class TextureLoader;
-    friend class MeshRenderer;
+    friend class StaticMeshComponent;
 
   private:
     vkg::resources::Image m_image;
