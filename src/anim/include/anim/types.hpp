df14a16e77c35e529a910b7f435407603f4705d8
Léon <levi486@gmail.com>
2022-02-02 19:36:51 +0100

    Move types to common lib

---
+++ src/anim/include/anim/types.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <common/types.hpp>
+#include <cstdint>
+
+namespace aln
+{
+using BoneIndex = uint16_t;
+using NodeIndex = uint32_t;
+using TaskIndex = uint32_t;
+using PoseBufferIndex = uint8_t;
+} // namespace aln
