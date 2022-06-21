ee0d61cdb7076f139ff921532428b8dc5dca58b6
Léon <levi486@gmail.com>
2022-06-21 17:03:19 +0200

    Add EnkiTS as a submodule, and create a wrapper service for it

---
+++ src/common/include/common/threading/task_service.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <TaskScheduler.h>
+#include <cstdint>
+
+namespace aln
+{
+
+using ITaskSet = enki::ITaskSet;
+using TaskSetPartition = enki::TaskSetPartition;
+
+class TaskService
+{
+
+  private:
+    uint32_t m_numWorkers;
+
+    enki::TaskScheduler m_taskScheduler;
+
+  public:
+    TaskService() : m_numWorkers(std::thread::hardware_concurrency())
+    {
+        enki::TaskSchedulerConfig config;
+        config.numTaskThreadsToCreate = m_numWorkers; // TODO: -1 to exclude main thread ?
+
+        m_taskScheduler.Initialize(config);
+    }
+
+    void ScheduleTask(ITaskSet* pTask)
+    {
+        m_taskScheduler.AddTaskSetToPipe(pTask);
+    }
+
+    void WaitForTask(ITaskSet* pTask)
+    {
+        m_taskScheduler.WaitforTaskSet(pTask);
+    }
+
+    /// @brief Execute a parallel task and wait for the result
+    void ExecuteTask(ITaskSet* pTask)
+    {
+        ScheduleTask(pTask);
+        WaitForTask(pTask);
+    }
+};
+} // namespace aln
