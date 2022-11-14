e2bd577de0cb9fd5ebbf82e3dd2b9f27fb18b31a
Léon <levi486@gmail.com>
2022-11-14 23:57:12 +0100

    Clean up camera controller system

--- src/core/include/core/entity_systems/camera_controller.hpp
+++ src/core/include/core/entity_systems/camera_controller.hpp
@@ -28,17 +28,8 @@ class EditorCameraController : IEntitySystem
     void Update(const UpdateContext& context) override;
 
   private:
-    aln::input::InputContext m_inputContext;
-
     float m_rotationSensitivity = 0.1f;
     float m_translationSensitivity = 0.006f;
     Camera* m_pCameraInstance = nullptr;
-
-    bool m_hasChanged = true; // default to true to trigger camera alignment on startup
-
-    /// @brief Translate the camera position on the 2D plane parallel to the screen.
-    void Move(aln::input::CallbackContext context);
-    void Rotate(aln::input::CallbackContext context);
-    void Zoom(aln::input::CallbackContext context);
 };
 } // namespace aln
