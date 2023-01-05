08f76d2c07a78ea212eb843aef7c663919ca0026
Léon <levi486@gmail.com>
2023-01-05 01:30:16 +0100

    Add dependency xxhash + Create aln hash library + Create StringID class

---
+++ src/common/include/common/serialization/hash.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <xxhash.h>
+
+namespace aln
+{
+namespace hash
+{
+constexpr static uint32_t Seed = 'ALNH';
+}
+inline static uint32_t Hash32(const std::string& str) { return XXH32(str.c_str(), str.size(), hash::Seed); }
+inline static uint32_t Hash32(const char* str) { return XXH32(str, strlen(str), hash::Seed); }
+/// ...
+
+} // namespace aln
---
+++ src/common/include/common/string_id.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "serialization/hash.hpp"
+
+namespace aln
+{
+class StringID
+{
+  private:
+    uint32_t m_hash;
+
+  public:
+    StringID() : m_hash(0) {}
+    StringID(const std::string& str) : m_hash(Hash32(str)) {}
+    StringID(const char* str) : m_hash(Hash32(str)) {}
+    StringID(uint32_t hash) : m_hash(hash) {}
+
+    inline bool IsValid() const { return m_hash != 0; }
+    inline uint32_t GetHash() const { return m_hash; }
+
+    bool operator==(const StringID& id) const { return m_hash == id.m_hash; }
+    bool operator!=(const StringID& id) const { return m_hash != id.m_hash; }
+    bool operator<(const StringID& id) const { return m_hash < id.m_hash; }
+};
+} // namespace aln
+
+namespace std
+{
+template <>
+struct hash<aln::StringID>
+{
+    size_t operator()(const aln::StringID& id) const { return id.GetHash(); }
+};
+} // namespace std
