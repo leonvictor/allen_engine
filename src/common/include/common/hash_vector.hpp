65bc18497a7e56f1e59956aa754269260bca9b04
Léon <levi486@gmail.com>
2023-08-09 21:36:03 +0200

    Add some utility functions to hash_vector

--- src/common/include/common/hash_vector.hpp
+++ src/common/include/common/hash_vector.hpp
@@ -9,16 +9,19 @@ namespace aln
 
 /// @brief Contiguous container with fast lookup.
 /// @todo Use a custom iterator keeping storing current index. Slight cost for performance improvements in case of find() -> erase() for example
+/// @todo Replace std containers
 template <typename ValueType, typename Hash = std::hash<ValueType>>
 class HashVector
 {
     using Iterator = std::vector<ValueType>::iterator;
+    using ConstIterator = std::vector<ValueType>::const_iterator;
     using KeyType = std::invoke_result_t<Hash, ValueType>;
+    using SizeType = std::vector<ValueType>::size_type;
 
   private:
     Hash hasher;
     std::vector<ValueType> m_vector;
-    std::unordered_map<KeyType, size_t> m_lookupMap;
+    std::unordered_map<KeyType, SizeType> m_lookupMap;
 
   public:
     size_t Size() const { return m_vector.size(); }
@@ -48,19 +51,72 @@ class HashVector
         return m_vector[it->second];
     }
 
+    ValueType& EmplaceBack()
+    {
+        auto& element = m_vector.emplace_back();
+        m_lookupMap[hasher(element)] = m_vector.size() - 1;
+        return element;
+    }
+
+    /// @brief Erase an element. Element ordering is not preserved !
+    Iterator Erase(Iterator it)
+    {
+        m_lookupMap.erase(hasher(*it));
+
+        if (it != m_vector.end() - 1)
+        {
+            // Move the last element in the vector to the erased element idx
+            *it = std::move(m_vector.back());
+            // Update the lookup map
+            m_lookupMap[hasher(*it)] = std::distance(m_vector.begin(), it);
+            m_vector.pop_back();
+            return it;
+        }
+        else
+        {
+            m_vector.pop_back();
+            return m_vector.end();
+        }
+    }
+
     void Erase(const KeyType& key)
     {
-        auto it = m_lookupMap.find(key);
-        m_vector.erase(m_vector.begin() + it->second);
-        m_lookupMap.erase(it);
+        auto it = m_vector.begin() + m_lookupMap[key];
+        Erase(it);
     }
 
     void Erase(const ValueType& value) { Erase(hasher(value)); }
 
+    /// @brief Erase all elements matching the predicate. Element order is not preserved !
+    template <typename Predicate>
+    void EraseIf(Predicate predicate)
+    {
+        // TODO: Group "erase" calls in a single statement
+        auto it = m_vector.begin();
+        while (it != m_vector.end())
+        {
+            if (predicate(*it))
+            {
+                it = Erase(it);
+            }
+            else
+            {
+                ++it;
+            }
+        }
+    }
+
     Iterator begin() { return m_vector.begin(); }
     Iterator end() { return m_vector.end(); }
+    ConstIterator begin() const { return m_vector.begin(); }
+    ConstIterator end() const { return m_vector.end(); }
+    ConstIterator cbegin() const { return m_vector.cbegin(); }
+    ConstIterator cend() const { return m_vector.cend(); }
 
-    ValueType& Get(const KeyType& key) { return m_vector[m_lookupMap[key]]; }
+    ValueType& Get(const KeyType& key) { return m_vector[m_lookupMap.at(key)]; }
+    const ValueType& Get(const KeyType& key) const { return m_vector[m_lookupMap.at(key)]; }
+    ValueType& operator[](SizeType index) { return m_vector[index]; }
+    const ValueType& operator[](SizeType index) const { return m_vector[index]; }
 };
 
 // TODO: Clients do no need this
