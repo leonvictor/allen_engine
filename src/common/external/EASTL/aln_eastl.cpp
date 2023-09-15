f13f010806283052b9d1da3766d29f6118316d6b
Léon <levi486@gmail.com>
2023-09-15 13:54:41 +0200

    Add eastl

---
+++ src/common/external/EASTL/aln_eastl.cpp
@@ -0,0 +1,27 @@
+#include "memory.hpp"
+
+#include <EASTL/allocator.h>
+
+// https://github.com/electronicarts/EASTL/blob/master/doc/FAQ.md#cont8-how-do-i-assign-a-custom-allocator-to-an-eastl-container
+namespace eastl
+{
+/// Defines a static default allocator which is constant across all types.
+allocator StaticDefaultAllocator;
+
+EASTL_API allocator* GetDefaultAllocator() { return &StaticDefaultAllocator; }
+EASTL_API allocator* SetDefaultAllocator(allocator* pAllocator) { return &StaticDefaultAllocator; }
+} // namespace eastl
+
+namespace eastl
+{
+allocator::allocator(const char* EASTL_NAME(pName)) {}
+allocator::allocator(const allocator& EASTL_NAME(alloc)) {}
+allocator::allocator(const allocator&, const char* EASTL_NAME(pName)) {}
+allocator& allocator::operator=(const allocator& EASTL_NAME(alloc)) { return *this; }
+const char* allocator::get_name() const { return EASTL_ALLOCATOR_DEFAULT_NAME; }
+void allocator::set_name(const char* EASTL_NAME(pName)) {}
+void* allocator::allocate(size_t n, int flags) { return aln::Allocate(n, EASTL_ALLOCATOR_MIN_ALIGNMENT); }
+void* allocator::allocate(size_t n, size_t alignment, size_t offset, int flags) { return aln::Allocate(n, alignment); }
+void allocator::deallocate(void* p, size_t) { aln::Free(p); }
+bool operator==(const allocator&, const allocator&) { return true; }
+} // namespace eastl
---
+++ src/common/include/common/containers/vector.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <EASTL/vector.h>
+
+namespace aln
+{
+	template<typename T>
+	using Vector = eastl::vector<T>;
+}
