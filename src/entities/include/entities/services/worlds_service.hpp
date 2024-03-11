987eb81d0370d871412af721200c1078228b8dcd
Léon <levi486@gmail.com>
2024-03-11 10:47:12 +0100

    Introduce a new service to manage existing worlds

---
+++ src/entities/include/entities/services/worlds_service.hpp
@@ -0,0 +1,78 @@
+#pragma once
+
+#include "../world_entity.hpp"
+
+#include <common/containers/vector.hpp>
+#include <common/services/service.hpp>
+
+namespace aln
+{
+/// @brief Keeps track of active worlds. Only one game world is active a any time, the others might be used by editor previews for example.
+class WorldsService : public IService
+{
+    friend class ServiceProvider;
+
+  private:
+    Vector<WorldEntity*> m_worlds;
+    uint32_t m_gameWorldIdx = InvalidIndex;
+
+  private:
+    void Shutdown() override
+    {
+        for (auto pWorld : m_worlds)
+        {
+            pWorld->Shutdown();
+            aln::Delete(pWorld);
+        }
+        m_worlds.clear();
+        m_gameWorldIdx = InvalidIndex;
+    }
+
+  public:
+    const Vector<WorldEntity*>& GetWorlds() const { return m_worlds; }
+
+    WorldEntity* GetGameWorld() { return m_worlds[m_gameWorldIdx]; }
+    const WorldEntity* GetGameWorld() const { return m_worlds[m_gameWorldIdx]; }
+
+    WorldEntity* CreateWorld(bool isGameWorld)
+    {
+        // Only one game world allowed
+        assert(!isGameWorld || m_gameWorldIdx == InvalidIndex);
+
+        auto pWorld = aln::New<WorldEntity>();
+        pWorld->Initialize(*GetProvider());
+        m_worlds.push_back(pWorld);
+
+        if (isGameWorld)
+        {
+            m_gameWorldIdx = m_worlds.size() - 1;
+        }
+
+        return pWorld;
+    }
+
+    void DestroyWorld(WorldEntity* pWorld)
+    {
+        const auto it = m_worlds.erase_first(pWorld);
+        pWorld->Shutdown();
+        aln::Delete(pWorld);
+    }
+
+
+    void UpdateLoading()
+    {
+        for (auto& pWorld : m_worlds)
+        {
+            pWorld->UpdateLoading();
+        }
+    }
+
+    void Update(const UpdateContext& ctx)
+    {
+        for (auto& pWorld : m_worlds)
+        {
+            pWorld->Update(ctx);
+        }
+    }
+};
+} // namespace aln
