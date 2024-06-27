a384d40fde9db6009335875cfb301244e9285e52
Léon <levi486@gmail.com>
2024-06-27 19:11:45 +0200

    Add compile-time conditions to select the right malloc methods

--- src/common/include/common/memory.hpp
+++ src/common/include/common/memory.hpp
@@ -14,7 +14,13 @@ namespace aln
 
 inline void* Allocate(size_t size, size_t alignment = 8)
 {
+
+#ifdef _WIN32
     auto ptr = _aligned_malloc(size, alignment);
+#else
+    auto ptr = std::aligned_alloc(alignment, size);
+#endif
+
 #ifdef ALN_DEBUG
     TracyAllocS(ptr, size, 15);
 #endif
@@ -26,19 +32,32 @@ inline void Free(void* ptr)
 #ifdef ALN_DEBUG
     TracyFreeS(ptr, 15);
 #endif
+
+#ifdef _WIN32
     _aligned_free(ptr);
+#else
+    std::free(ptr);
+#endif
 }
 
 #ifdef ALN_DEBUG
 // Simple version with no marking to be used by external libraries
 inline void* AllocateUnmarked(size_t size, size_t alignment = 8)
 {
+#ifdef _WIN32
     return _aligned_malloc(size, alignment);
+#else
+    return std::aligned_alloc(alignment, size);
+#endif
 }
 
 inline void FreeUnmarked(void* ptr)
 {
+#ifdef _WIN32
     _aligned_free(ptr);
+#else
+    std::free(ptr);
+#endif
 }
 #endif
 
