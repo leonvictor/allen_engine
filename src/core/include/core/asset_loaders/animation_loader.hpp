7526c6f048a67e7a25ffec1001c987dbfc3cb54a
Léon <levi486@gmail.com>
2024-08-09 22:01:06 +0200

    Fixes

--- src/core/include/core/asset_loaders/animation_loader.hpp
+++ src/core/include/core/asset_loaders/animation_loader.hpp
@@ -45,7 +45,7 @@ class AnimationLoader : public IAssetLoader
         pAnim->m_syncTrack = SyncTrack::Default;
 
         // TODO: Add a dependency on the skeleton, and ensure it is loaded correctly
-        
+
         pRecord->SetAsset(pAnim);
         return true;
     }
@@ -57,7 +57,7 @@ class AnimationLoader : public IAssetLoader
 
         auto pSkeletonRecord = GetDependencyRecord(dependencies, 0);
         pAnimClip->m_pSkeleton = AssetHandle<Skeleton>(pSkeletonRecord->GetAssetID());
-        pAnimClip->m_pSkeleton.m_pAssetRecord = pSkeletonRecord;
+        UpdateDependencyRecord(pAnimClip->m_pSkeleton, pSkeletonRecord);
     }
 };
 } // namespace aln
